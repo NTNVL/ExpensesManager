a1c88dedcb6e093f15fa83bfb501474178e6273a
Nathãn <nathan2001lima@gmail.com>
2020-11-22 15:27:41 -0300

    Added input to add a expense to table

--- components/ExpenseTable.tsx
+++ components/ExpenseTable.tsx
@@ -12,7 +12,8 @@ type Props = {
 }
 
 type State = {
-  expenses: Expense[]
+  expenses: Expense[],
+  newExpense: Expense
 }
 
 const ExpenseRow = (props: { expense: Expense }) => (
@@ -24,14 +25,29 @@ const ExpenseRow = (props: { expense: Expense }) => (
 
 
 export class ExpenseTable extends React.Component<Props, State> {
-  public readonly state: State = { expenses: [] }
+  public readonly state: State = {
+    expenses: [],
+    newExpense: {
+      description: "",
+      value: 0
+    }
+  }
   title: string;
 
   constructor(props: Props) {
     super(props);
 
     this.title = props.title;
-    this.state = { expenses: props.expenses };
+    this.state = {
+      expenses: props.expenses,
+      newExpense: {
+        description: "",
+        value: 0
+      }
+    };
+
+    this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
+    this.handleValueChange = this.handleValueChange.bind(this);
   }
 
   getTotalExpenses(): number {
@@ -44,8 +60,27 @@ export class ExpenseTable extends React.Component<Props, State> {
   addExpense(expense: Expense) {
     let new_expenses = this.state;
     new_expenses.expenses.push(expense);
+    new_expenses.newExpense = {description: "", value: 0}
 
     this.setState(new_expenses);
+  }
+
+  handleDescriptionChange(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({
+      newExpense: {
+        description: event.target.value,
+        value: this.state.newExpense.value
+      }
+    })
+  }
+
+  handleValueChange(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({
+      newExpense: {
+        description: this.state.newExpense.description,
+        value: event.target.valueAsNumber
+      }
+    })
   }
 
   render() {
@@ -62,15 +97,44 @@ export class ExpenseTable extends React.Component<Props, State> {
     />
 
     return (
-      <table className={ styles.table }>
-        <thead>
-          { titleRow }
-        </thead>
-        <tbody>
-          { expensesRows }
-          { totalRow }
-        </tbody>
-      </table>
+      <div>
+        <table className={ styles.table }>
+          <thead>
+            { titleRow }
+          </thead>
+          <tbody>
+            { expensesRows }
+            { totalRow }
+          </tbody>
+          <tfoot>
+            <tr>
+              <td>
+                <input
+                  id="getDescription"
+                  type="text"
+                  placeholder="Descrição"
+                  value={this.state.newExpense.description}
+                  onChange={this.handleDescriptionChange}>
+                </input>
+              </td>
+              <td>
+                <input
+                  id="getValue"
+                  type="number"
+                  placeholder="Valor"
+                  min="0"
+                  step="0.1"
+                  value={this.state.newExpense.value}
+                  onChange={this.handleValueChange}>
+                </input>
+              </td>
+              <button
+                onClick={(e) => this.addExpense(this.state.newExpense)}>+
+              </button>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
     );
   }
 }
